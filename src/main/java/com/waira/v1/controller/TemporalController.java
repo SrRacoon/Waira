e889d0eecba39bfbce334538dd0553d95a70d842
SrRacun <lt.antonio.p@gmail.com>
2025-10-01 00:56:26 -0500

    feat: Update temporal view to display full user information and remove unused HTML templates

--- src/main/java/com/waira/v1/controller/TemporalController.java
+++ src/main/java/com/waira/v1/controller/TemporalController.java
@@ -3,6 +3,7 @@ package com.waira.v1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// SOLO ES TEMPORAL MIENTRAS SE HACEN LAS DEMÁS VISTAS
 @Controller
 public class TemporalController {
     @GetMapping("/temporal")
